8964a1106fe41ab097e2593fcaaf4773eb3dc673
Ayuba Tahiru <58880393+Tayuba@users.noreply.github.com>
2021-09-27 23:22:51 +0200

    day 1
    show, capture, videos and images

---
+++ day1_read_show_captue_picture_vidoe.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+"""Load and read image"""
+img = cv2.imread("images/ayuba_profile_pic.png", -1)
+
+"""Show image"""
+# cv2.imshow("ayuba",  img)
+# cv2.waitKey(10qq
+
+"""Showing video"""
+frameWidth = 1280
+frameHeight = 720
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    sucess, imag = cap.read()
+    imag = cv2.resize(imag, (frameWidth, frameHeight))
+    cv2.imshow("vidoe", imag)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
